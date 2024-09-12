86a160c87954913b22c88bedc170d9b7e4976637
lazy_app <a.raheeem0003@gmail.com>
2024-09-12 23:02:16 +0100

    Finished navigation routing

---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import {ReactComponent as Baglogo} from '../../assets/Baglogo.svg'
+import './Navigation.scss'
+
+const Navigation = () => {
+    return(
+      <Fragment>
+        <div className='navigation'>
+          <Link className='logo-container' to="/">
+          <Baglogo className='logo'/>
+          </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>Shop</Link>
+          </div>
+        </div>
+        <Outlet/>
+      </Fragment>
+    )
+  }
+  
+
+export default Navigation
